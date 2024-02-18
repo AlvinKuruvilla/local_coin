c3413d8ee2a067bd30130dbf927e95250cc4f508
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2024-02-17 20:56:38 -0500

    Remove the subxt dependency
    We'll circle back to adding transactions to the node if we have
    to once everything else is more concrete

--- local_coin/src/shim/contract_runner.rs
+++ local_coin/src/shim/contract_runner.rs
@@ -1,7 +1,3 @@
-use crate::bigquery_transactions::parse_as_records;
-use anyhow::Result;
-use subxt::{OnlineClient, PolkadotConfig};
-use subxt_signer::sr25519::dev;
 use sysinfo::System;
 
 pub fn is_substrate_node_running() -> bool {
@@ -14,40 +10,4 @@ pub fn is_substrate_node_running() -> bool {
         return true;
     }
     false
-}
-
-// Generate an interface that we can use from the node's metadata.
-#[subxt::subxt(runtime_metadata_path = "metadata.scale")]
-pub mod polkadot {}
-
-pub async fn submit_transactions(file_path: String) -> Result<()> {
-    let api = OnlineClient::<PolkadotConfig>::new().await?;
-    let recs = parse_as_records(file_path).unwrap();
-    // TODO: The end goal is to use outputs_addresses field for this, but kept getting
-    //       Phrase(BadWordCount(1))
-    //       Keypair::from_uri(&SecretUri::from_str(&rec.dest()).unwrap())
-    //       .unwrap()
-    //       .public_key()
-    //       .into(),
-    let dest = dev::bob().public_key().into();
-    let balance_transfer_tx = polkadot::tx().balances().transfer_allow_death(dest, 10_000);
-    // TODO: The end goal is the same as above except with input addresses
-    //       &Keypair::from_uri(&SecretUri::from_str(&rec.from()).unwrap()).unwrap(),
-    let from = dev::alice();
-    println!("Here");
-    let progress = api
-        .tx()
-        .sign_and_submit_then_watch_default(&balance_transfer_tx, &from)
-        .await?;
-    println!("Are we Here?");
-
-    let events = progress.extrinsic_hash();
-    println!("Not Here");
-    // Find a Transfer event and print it.
-    // let transfer_event = events.find_first::<polkadot::balances::events::Transfer>()?;
-    // if let Some(event) = transfer_event {
-    //     println!("Balance transfer success: {event:?}");
-    // }
-
-    Ok(())
 }
